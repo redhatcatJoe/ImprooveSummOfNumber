7ff690243ecf864638d23688e28fd62e0059fa25
redhatcatJoe <redhatcatjoe@gmail.com>
2019-10-01 19:48:27 +0500

    yo

---
+++ .idea/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Summ su = new Summ ();
+        su.logic ();
+        System.out.println ("сумма чмсел ="+su.summ () );
+
+//        System.out.println (1234%10 ); // last
+//        System.out.println (1234%100/10 ); //
+//        System.out.println (1234%1000/100);//
+//        System.out.println (1234%10000/1000 ); //first
+
+
+
+    }
+}
+
---
+++ .idea/src/Summ.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Summ {
+
+    int  numOfNumbersInNumber = 0;
+    ArrayList<Integer> numList = new ArrayList <> ();
+
+
+    void logic(){                                     //метод узнает разрядность числа и заносит каждое число в коллекцию
+        Scanner scanner = new Scanner (System.in);
+        System.out.println ("input number" );
+        int s = Integer.parseInt (scanner.nextLine ());
+        numOfNumbersInNumber = (int) Math.log10(s)+1;   //узнаем разрядность
+        //десятичный логарифм - в какую степень нужно возвести 10 что бы получить s +1
+        //int отбрасывает знаки после запятой
+        System.out.println ("разрядность числа="+numOfNumbersInNumber );
+
+        for (int i = 1; i <=numOfNumbersInNumber ; i++) {
+            switch (i){
+                case 1:
+                    numList.add (s%( (int)Math.pow (10,i)));
+//                    System.out.println ("case1" );
+//                    System.out.println (numList.get (0) );
+//                    System.out.println ("i="+i );
+                    break;
+                case 2:
+                    numList.add (s%( (int)Math.pow (10,i))/10);
+//                    System.out.println ("case2" );
+//                    System.out.println (numList.get (1) );
+//                    System.out.println ("i="+i );
+
+                    break;
+                default:
+                    numList.add (s%( (int)Math.pow (10,i))/((int)Math.pow (10,i-1)));
+//                    System.out.println ("default" );
+//                    System.out.println (numList.get (i-1 ));
+//                    System.out.println ("i="+i );
+                    break;
+            }
+        }
+    }
+
+
+    int summ(){                                                   //метод берет числа из коллекции и складывает их
+        int s=0 ;
+        for (int i = 0; i <numList.size () ; i++) {
+
+            s+= numList.get (i);
+
+        }
+        return s;
+    }
+
+
+}
+
