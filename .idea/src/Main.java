16ffb76273d49c9944234520fc65f39eecd156e6
redhatcatJoe <redhatcatjoe@gmail.com>
2019-10-01 21:34:09 +0500

    yo

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-//        Summ su = new Summ ();
-//        su.logic ();
-//        System.out.println ("сумма чмсел ="+su.summ () );
+        Summ su = new Summ ();
+        su.logic ();
+        System.out.println ("сумма чмсел ="+su.summ () );
 
-      System.out.println (1234%10 ); // last   4
+//        System.out.println (1234%10 ); // last   4
 //        System.out.println (1234%100/10 ); //3
 //        System.out.println (1234%1000/100);//2
 //        System.out.println (1234%10000/1000 ); //first 1
